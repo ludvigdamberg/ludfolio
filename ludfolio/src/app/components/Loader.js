a9e1c07a3be5dbd0a2448b564e0148afeffff06d
ludvigdamberg <ludda756@student.liu.se>
2024-01-10 21:26:51 +0100

    Update Loader.js

--- ludfolio/src/app/components/Loader.js
+++ ludfolio/src/app/components/Loader.js
@@ -13,7 +13,7 @@ function Loader() {
 
     tl2.to(".heart", { delay:0.5, duration:0.5, y:300 ,ease: "back.in"  })
     tl.fromTo(".load",{y:120}, {delay:0.5, duration: 0.5, y: 0, ease: "back.out", opacity: 1, stagger: 0.1 })
-    tl.to(loader.current,{ height: 0, duration: 0.4,ease: "power1.in"})
+    tl.to(loader.current,{ height: 0, duration: 0.5,ease: "power3.in"})
    
   }, [])
 
