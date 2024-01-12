8bcd4ae891be22edd07b08268190c22a743e0b9d
ludvigdamberg <ludda756@student.liu.se>
2024-01-13 00:15:25 +0100

    Update Loader.js

--- ludfolio/src/app/components/Loader.js
+++ ludfolio/src/app/components/Loader.js
@@ -11,19 +11,19 @@ function Loader() {
     const tl = gsap.timeline()
     const tl2 = gsap.timeline()
     tl2.to(".heart", { delay:0.1, duration:0.5, y:300 ,ease: "back.in"  })
-    tl.fromTo(".load",{y:120}, {delay:0.1, duration: 0.5, y: 0, ease: "back.out", opacity: 1, stagger: 0.1 })
-    tl.to(loader.current,{ height: 0, duration: 0.5,ease: "power3.in"})
+    tl.fromTo(".load",{y:-150}, {delay:0.1, duration: 0.7, y: 0, ease: "back.out", opacity: 1, stagger: 0.1 })
+    tl.to(loader.current,{ height: 0, duration: 0.5,delay:0.2 ,ease: "power3.in"})
    
   }, [])
 
   return (
     <div ref={loader} className=' h-screen w-screen bg-neutral-950 text-neutral-200 fixed top-0 left-0 z-50 text-5xl  justify-center items-center flex overflow-hidden'>
       <div className='flex h-auto'>
-        <div className=' flex flex-col md:text-9xl text-5xl border rounded-l-xl md:h-[250px] md:w-[250px] overflow-hidden'>
-          <p className='load opacity-0 h-auto tracking-tight overflow-hidden'>LUD</p>
-          <p className='load tracking-widest opacity-0 h-auto overflow-hidden'>VIG</p>
+        <div className=' border-t border-r flex flex-col md:text-9xl text-5xl text-center  md:h-[250px] md:w-[250px] overflow-hidden'>
+          <p className='load opacity-0 h-auto tracking-tight overflow-hidden text-center'>LUD</p>
+          <p className='load tracking-widest opacity-0 h-auto overflow-hidden text-center'>VIG</p>
         </div>
-        <div className='  border rounded-r-xl border-neutral-50 md:w-[250px] text-5xl md:text-9xl md:h-[250px] overflow-hidden flex  '><FaHourglass className='heart my-auto md:mx-auto mx-6'/> </div>
+        <div className=' border-b  border-neutral-50 md:w-[250px] text-5xl md:text-9xl md:h-[250px] overflow-hidden flex  '><FaHourglass className='heart my-auto md:mx-auto mx-6'/> </div>
       </div>
     </div>
   )
