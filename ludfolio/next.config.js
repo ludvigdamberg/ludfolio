562e30d4303293d187adfeac91035e8fc8861b0b
Ludvig Damberg <ludvig.damberg@outlook.com>
2024-05-15 10:37:19 +0200

    Update next.config.js

--- ludfolio/next.config.js
+++ ludfolio/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true,
+  images: {
+    unoptimized: true,
+  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'cdn.sanity.io',
+      port: '',
     },
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn.sanity.io',
-            port: '',
-          },
-        ],
-      },
+  ],
 }
+
 
 module.exports = nextConfig
